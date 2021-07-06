ca701a54650e6c441ad911302107d59ea42e3452
shannon_p <snp1284kasey@aol.com>
2021-07-06 09:03:53 -0400

    Updated the form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 // Stop at 4.2.4 right at Add task type dropdown
 
-var buttonE1 = document.querySelector("#save-task");
-// console.log(buttonE1);
-
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task";
     tasksToDoE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
 //  {
 //     // 1. Created a new HTML element specified by tagname 
 //     var listItemE1 = document.createElement("li");
