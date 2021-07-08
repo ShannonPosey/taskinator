fb9c9d4a15a74019cd67d19432adba87ee7575e9
shannon_p <snp1284kasey@aol.com>
2021-07-08 05:43:08 -0400

    Add the ability to edit a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // Stop at 4.2.8 right at Reset the form
+var taskIdCounter = 0;
 
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
-var taskIdCounter = 0;
+var pageContentE1 = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
 
@@ -94,7 +95,54 @@ var createTaskActions = function(taskId) {
     return actionContainerE1;
 };
 
+var taskButtonHandler = function(event) {
+    // get target element from event
+    var targetE1 = event.target;
+    console.log(event.target);
+
+    // edit button was clicked
+    if (targetE1.matches(".edit-btn")) {
+        var taskId = targetE1.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+
+    // delete button was clicked
+    else if (targetE1.matches(".delete-btn")) {
+        // get the element's task id
+        var taskId = targetE1.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var editTask = function(taskId) {
+    console.log( taskId);
+
+    // get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    console.log(taskName);
+    
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    console.log(taskType);
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formE1.setAttribute("data-task-id", taskId);
+};
+
+var deleteTask = function(taskId) {
+    console.log(taskId);
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+pageContentE1.addEventListener("click", taskButtonHandler);
 formE1.addEventListener("submit", taskFormHandler);
+
 //  {
 //     // 1. Created a new HTML element specified by tagname 
 //     var listItemE1 = document.createElement("li");
